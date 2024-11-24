3650a85d6592bca5b65d50b71eff077b93e4c9b5
Starinin Andrey <starinin-andrey@ya.ru>
2024-11-24 16:23:30 +0300

    Initial commit

---
+++ AvaloniaApplication1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AvaloniaApplication1.Models;
+
+public class Person
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    
+    [JsonPropertyName("age")]
+    public int Age { get; set; }
+}
